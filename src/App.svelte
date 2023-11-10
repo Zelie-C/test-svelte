82e13546e10e311b3c7bc61bbfca3d3fec198c28
Zelie Chemin <zelie.chemin@gmail.com>
2023-11-10 09:52:54 +0100

    fin boutons

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  let count: number = 0
+
+  const increment = () => count += 1
+  const decrement = () => count -= 1
+
+
+</script>
+
+<main>
+
+  <h1>Compteur avec Svelte</h1>
+
+  <div class="card">
+    <Counter {count} {increment} />
+    <button class="button button-parent" on:click={decrement}>Parent Decrement Button</button>
+  </div>
+
+
+</main>
+
+<style>
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let count: number
+  export let increment: () => void
+
+</script>
+
+<button on:click={increment}>
+  Increment : {count}
+</button>
